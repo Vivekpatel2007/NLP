567197a5695d3eed68b93576d867ee8e710b261d
Vivek Patel <viveknp2007@gmail.com>
2026-08-07 01:36:32 +0530

    Delete ASSIGNMENT_1 directory

--- ASSIGNMENT_1/parquet_to_txt.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_parquet('ASSIGNMENT_1/tokenized_gujarati_corpus/gujarati_tokenized_batch_0000.parquet') # Output of Tokenizer.py
-df.to_csv('ASSIGNMENT_1/out.csv', index=False)             
-df.to_csv('ASSIGNMENT_1/out.txt', index=False, sep='\t')  
-
-df.to_excel('output.xlsx', index=False) # Save the DataFrame to an Excel (XLSX) file
